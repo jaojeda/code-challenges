3373891096fceaced56b56b873f7561aa528aa2f
Jose <j.a.ojeda@live.com>
2020-07-23 22:52:44 -0700

    initial code challenge set up

---
+++ array-manipulation/applicationState.js
@@ -0,0 +1,73 @@
+class ApplicationState {
+  constructor() {
+    let stepOne = {
+      name: 'Step 1',
+      status: 'active',
+      tasks: [{
+        name: 'Task A',
+        status: 'active'
+      }, {
+        name: 'Task B',
+        status: 'pristine'
+      }, {
+        name: 'Task C',
+        status: 'pristine'
+      }]
+    };
+
+    let stepTwo = {
+      name: 'Step 2',
+      status: 'pristine',
+      tasks: [{
+        name: 'Task D',
+        status: 'pristine'
+      }]
+    };
+
+    let stepThree = {
+      name: 'Step 3',
+      status: 'pristine',
+      tasks: [{
+        name: 'Task E',
+        status: 'pristine'
+      }, {
+        name: 'Task F',
+        status: 'pristine'
+      }, {
+        name: 'Task G',
+        status: 'pristine'
+      }]
+    };
+
+
+    this.steps = [
+      stepOne,
+      stepTwo,
+      stepThree
+    ];
+  }
+
+  // Returns the step name and task name of the currently active task
+  getActiveTask() {
+
+  }
+
+  // Sets the provided step and task to 'active', 
+  // all preceding steps and tasks to 'complete',  
+  // and all future steps and tasks to 'pristine'.
+  setActiveTask(stepName, taskName) {
+
+  }
+
+  // Gets the step name and task name for the task immediately after the currently active task
+  getNextTask() {
+ 
+  }
+
+  // Gets the step name and task name for the task immediately before the currently active task
+  getPreviousTask() {
+ 
+  }
+}
+
+let state = new ApplicationState();
