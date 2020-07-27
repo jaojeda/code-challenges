8644c740aba26030ca6615fa244190779f4892e1
Jose <j.a.ojeda@live.com>
2020-07-27 12:46:48 -0700

    added compareTriplets

---
+++ array-manipulation/compareTriplets.js
@@ -0,0 +1,9 @@
+/* The task is to find their comparison points by comparing a[0] with b[0], a[1] with b[1], and a[2] with b[2].
+ If a[i] > b[i], then Alice is awarded 1 point. If a[i] < b[i], then Bob is awarded 1 point. If a[i] = b[i], then neither person receives a point.
+ Return output should be array with sore of a at index 0, score of b at index 1.
+ */
+
+
+function compareTriplets(a, b) {
+
+}
