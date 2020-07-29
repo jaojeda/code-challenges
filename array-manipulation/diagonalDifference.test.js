f86fea47bd1fc35b771102c322cd81472fdd7e06
Jose <j.a.ojeda@live.com>
2020-07-28 23:54:19 -0700

    wrote third test for diagonalDifference

--- array-manipulation/diagonalDifference.test.js
+++ array-manipulation/diagonalDifference.test.js
@@ -14,4 +14,12 @@ describe('diagonalDifference', () => {
 
     expect(diagonalDifference(matrix)).toEqual(15);
   })
+
+  it('deals with any size square matrix', () => {
+    const matrixA = [[0, 1], [2, 3]]
+    const matrixB = [[1, 2, 3, 4], [5, 6, 7, 8], [1, 2, 3, 4], [8, 7, 6, 0]]
+
+    expect(diagonalDifference(matrixA)).toEqual(0);
+    expect(diagonalDifference(matrixB)).toEqual(11);
+  })
 });
