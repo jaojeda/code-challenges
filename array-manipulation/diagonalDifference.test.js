81fd93397b3dbdb58e1fac85194061327f94ee45
Jose <j.a.ojeda@live.com>
2020-07-28 23:49:03 -0700

    wrote second test for diagonalDifference

--- array-manipulation/diagonalDifference.test.js
+++ array-manipulation/diagonalDifference.test.js
@@ -4,9 +4,14 @@ const { diagonalDifference } = require('./diagonalDifference');
 describe('diagonalDifference', () => {
 
   it('returns the difference of a 3 by 3 square matrix', () => {
-    const matrix = [[1, 2, 3], [4, 5, 6], [9, 8, 9]]
+    const matrix = [[1, 2, 3], [4, 5, 6], [9, 8, 9]];
     
-    expect(diagonalDifference(matrix)).toEqual(2)
-  })
+    expect(diagonalDifference(matrix)).toEqual(2);
+  });
 
-})
+  it('deal with negative numbers', () => { 
+    const matrix = [[11, 2, 4], [4, 5, 6], [10, 8, -12]];
+
+    expect(diagonalDifference(matrix)).toEqual(15);
+  })
+});
