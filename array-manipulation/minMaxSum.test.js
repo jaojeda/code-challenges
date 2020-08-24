3b6e6c3acd31203d0f6b62f9075a130f8bf69b6f
Jose <j.a.ojeda@live.com>
2020-08-23 21:41:25 -0700

    wrote test of minMaxSum.js

--- array-manipulation/minMaxSum.test.js
+++ array-manipulation/minMaxSum.test.js
@@ -1,3 +1,14 @@
-describe('Min Max Sum' () => {
+const { minMaxSum } = require('./minMaxSum');
+
+describe('Min Max Sum', () => {
   
-})
+  it('gets the min and max of an array of five digits', () => {
+    const arr = [1, 2, 3, 4, 5];
+
+    console.log = jest.fn();
+    minMaxSum(arr);
+
+    expect(console.log).toHaveBeenCalledWith('10 14');
+  });
+
+});
