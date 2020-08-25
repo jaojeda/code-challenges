084cde520f00bbe98f8bc2158c69af1ddbb133c2
Jose <j.a.ojeda@live.com>
2020-08-25 10:08:59 -0700

    set up for findTotalHighest

---
+++ array-manipulation/findTotalLargest.js
@@ -0,0 +1,8 @@
+//Find the total number of instances the largest number in an array of numbers.
+
+
+const findTotalLargest = (arr) => {
+  console.log(arr);
+};
+
+module.exports = { findTotalLargest };
