2d67027ff1fd37e9dc4f4820bf881ecb6dcbec7d
Jose <j.a.ojeda@live.com>
2020-08-26 12:08:47 -0700

    solved findTotalLargest

--- array-manipulation/findTotalLargest.js
+++ array-manipulation/findTotalLargest.js
@@ -1,8 +1,12 @@
 //Find the total number of instances the largest number in an array of numbers.
 
-
 const findTotalLargest = (arr) => {
-  console.log(arr);
+  return arr.sort((a, b) => b - a).reduce((acc, curr, i, src) => {
+    if(curr === src[i + 1]) {
+      acc++;
+    }
+    return acc;
+  }, 1);
 };
 
 module.exports = { findTotalLargest };
